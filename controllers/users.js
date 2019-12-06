312835b04afe83663c225bd5d3d7890a149d4565
Nikita <bogdanov.exceedteam@gmail.com>
2019-12-06 17:37:28 +0300

    async/await functions

--- controllers/users.js
+++ controllers/users.js
@@ -6,14 +6,21 @@ let bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 
-function login (req, res) {
-    User.findOne({ email: req.body.email }, function (err, data) {
-        if (data) {
-            bcrypt.compare(req.body.pw, data.pw, function(err, check) {
-                if (check === true) {
+const login = async (req, res) => {
+    try {
+        let currentUser = await new Promise((resolve, reject) => {
+            User.findOne({email: req.body.email}, function (err, data) {
+                if (err) reject(err);
+                else resolve(data);
+            })
+        });
+
+        if (currentUser) {
+            bcrypt.compare(req.body.pw, currentUser.pw, function (err, check) {
+                if (check) {
                     console.log("login");
                     var newSession = new Session({
-                        user_id: data._id,
+                        user_id: currentUser._id,
                         created_at: curDate(0),
                         updated_at: curDate(0),
                         expires_in: curDate(600),// 10 min
@@ -24,7 +31,6 @@ function login (req, res) {
                         })
                     });
                 } else {
-                    console.log("wrong pw");
                     res.status(500).json({
                         "error": "Wrong pw"
                     })
@@ -33,95 +39,113 @@ function login (req, res) {
         } else {
             console.log("wrong email");
             res.status(500).json({
-                error: "Wrong email"
+                "error": "Wrong email"
             })
         }
-        if (err) {
-            console.log("Something was wrong. Please ty again later");
-            return res.status(500).json({
-                "error": "Something was wrong. Please ty again later"
-            })
-        }
-    });
+    } catch (e) {
+        return res.status(500).json({
+            "error": "Something was wrong. Please ty again later"
+        })
+    }
 };
 
-const registration =  async (req, res) => {
-    // let  CurrentUser = await  User.findOne({ email: req.body.email });
-    // console.log(CurrentUser)
-    User.findOne({ email: req.body.email }, function (err, data) {
-        if (data) {
+const registration = async (req, res) => {
+    try {
+        let currentUser = await new Promise((resolve, reject) => {
+            User.findOne({email: req.body.email}, function (err, data) {
+                if (err) reject(err);
+                else resolve(data);
+            })
+        });
+
+        if (currentUser) {
             return res.status(500).json({
                 "message": `account with ${data.email} email already exist`
             })
-        }
-        if (err) {
-            return res.status(500).json({
-                "message": "Something was wrong. Please ty again later"
-            })
-        }
 
-        bcrypt.hash(req.body.pw, saltRounds, function(err, hash) {
-            let newUser = new User({
-                email: req.body.email,
-                pw: hash,
-            });
+        } else {
+            bcrypt.hash(req.body.pw, saltRounds, function (err, hash) {
+                let newUser = new User({
+                    email: req.body.email,
+                    pw: hash,
+                });
 
-            newUser.save(function (err, newUser) {
-                if (err) {
-                    return res.status(500).json({
-                        "message": "Something was wrong. Please ty again later"
+                newUser.save(function (err, newUser) {
+                    if (err) {
+                        return res.status(500).json({
+                            "message": "Something was wrong. Please ty again later"
+                        })
+                    }
+                    res.status(200).json({
+                        "message": "Registration complete!"
                     })
-                }
-                res.status(200).json({
-                    "message": "Registration complete!"
-                })
+                });
             });
+        }
+    } catch (e) {
+        return res.status(500).json({
+            "message": "Something was wrong. Please ty again later"
         });
-    });
+    }
 };
 
-function newList (req, res) {
-    Session.findOne({_id: req.body.session_id }, function (err, data) {
-        if (err) {
-            return res.status(500).json({
-                "message": "Something was wrong. Please ty again later"
-            })
-        }
-
+const newList = async (req, res) => {
+    try {
         let newList = new List({
             name: req.body.name,
-            user_id: data.user_id,
+            user_id: req.body.user_id,
         });
 
-        newList.save(function (err, newList) {
-            if (err) {
-                return res.status(500).json({
-                    "message": "Something was wrong. Please ty again later"
-                })
-            }
-            res.status(200).json({
-                "message": "New list created"
-            })
+        const saveList = await new Promise((resolve, reject) => {
+            newList.save(function (err, newList) {
+                console.log(err, newList)
+                    if (err) reject(err);
+                    else resolve(newList);
+                }
+            )
         });
-    });
+
+        if(saveList) {
+            console.log("New list created");
+            console.log("USER: " + req.body.user_id);
+            return  res.status(200).json({'new': 'new'})
+
+        }
+    } catch (e) {
+        return res.status(500).json({
+            "message": "Something was wrong. Please ty again later"
+        })
+    }
 };
 
-function testToken (req, res, next) {
-    Session.findOne({_id: req.body.session_id}, function (err, data) {
-        if (err) return res.end("token undefine", 503);
-        if (new Date() < data.expires_in) {
-            data.updated_at = curDate(0);
-            data.expires_in = curDate(600);// 10 min
+const testToken = async (req, res, next) => {
+    try {
+        let currentSession = await new Promise((resolve, reject) => {
+            Session.findOne({_id: req.body.session_id}, function (err, data) {
+                if (err) reject(err);
+                else resolve(data);
+            })
+        });
 
-            data.save(function (err, data) {
-                res.end(`${data._id} session updates ${curDate(0)}`, 200)
-            });
-
-            next(data.user_id);
+        if (currentSession) {
+            if (new Date() < currentSession.expires_in) {
+                currentSession.updated_at = curDate(0);
+                currentSession.expires_in = curDate(600);// 10 min
+                currentSession.save(function (err, data) { });
+                req.body.user_id = currentSession.user_id;
+                console.log("S");
+                next();
+            } else {
+                res.status(500).json({
+                    "message": "Token outdated"
+                })
+            }
         } else {
-            res.end("token outdated", 500)
+            return res.status(503).json({
+                "message": "Token undefine"
+            });
         }
-    });
+    } catch (e) { }
 }
 
 function curDate(sec) {
